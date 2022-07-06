b0b8c9ab37d938200c6ab37356f1bff20967bc9f
Your Name <you@example.com>
2022-07-06 22:52:17 +0200

    small changes

---
+++ src/test/java/Test18.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+
+// -c src/test/resources/testpar/restpar18.properties -p 7999
+
+
+public class Test18 extends PTestRestHelper  {
+
+    @Test
+    public void test1() throws IOException {
+        int res = makegetcall("/testput", null);
+        P("Method not allowed expected");
+        assertEquals(405, res);
+    }
+
+    @Test
+    public void test2() throws IOException {
+        int res = makegetcall("/testput", null);
+        P("Method not allowed expected");
+        assertEquals(405, res);
+    }
+
+}
